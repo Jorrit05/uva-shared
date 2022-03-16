7efe368cce4154c81c86f199cfa566b6fd463350
Rein Spanjer <reinfalke@gmail.com>
2022-03-16 19:32:02 +0100

    java playground

--- assignment3/java-playground/demo/src/main/java/com/playground/App.java
+++ assignment3/java-playground/demo/src/main/java/com/playground/App.java
@@ -4,14 +4,11 @@ package com.playground;
  * Hello world!
  *
  */
-public class App 
+class App 
 {
-    public static void main( String[] args )
+    static void main( String[] args )
     {
-        Boolean x = null;
         System.out.println( "Hello World!" );
-        boolean answer = true == true;
-        Boolean answer2 = Boolean.TRUE.equals(true);
-        boolean y = x == false;
     }
+
 }
